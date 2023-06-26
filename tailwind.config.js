1a783cd26a988fbeb40be8a93d3c3f91e6865180
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-27 01:32:27 +0700

    Config: Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss/types').Config} */
 const config = {
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
