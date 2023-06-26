582b1aef308bb503df90831a1d8c806e51743be7
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-27 01:35:50 +0700

    Modified: using layout to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Head from "next/head";
+import Layout from "../components/Templates/Layout";
 // import LandingPageTemplate from "../../templates/LandingPage/LandingPageTemplate";
 
 const Index: NextPage = () => {
@@ -7,8 +8,21 @@ const Index: NextPage = () => {
     <>
       <Head>
         <title>ghaly.dev</title>
+        <link
+          rel="icon"
+          href="https://user-images.githubusercontent.com/60167960/229319877-77600973-dc1f-4b2a-b0da-ed38a67eec0e.png"
+        />
         <meta name="description" content="Created by Rachmat Ghaly." />
+        <script
+          src="https://kit.fontawesome.com/0ae515efac.js"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
+      <Layout>
+        <div>
+          <p className="text-white">Page home ghaly</p>
+        </div>
+      </Layout>
       {/* <LandingPageTemplate /> */}
     </>
   );
