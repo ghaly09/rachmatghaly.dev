b98d09a3b9a1aa79423c8d2bf2b619d7a713cb74
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-27 01:36:46 +0700

    Modified: Dark Mode theme config in document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,9 @@ import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 export default class Document extends NextDocument {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className="dark">
         <Head></Head>
-        <body>
+        <body className="bg-white dark:bg-black">
           <Main />
           <NextScript />
         </body>
