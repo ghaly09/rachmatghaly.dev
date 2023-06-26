18887b83111d7b5b4f02fe46d82b978bf6b5202f
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-27 01:33:19 +0700

    Modified: change some props of button footer

--- src/components/Atoms/Buttons/Button-Footer.tsx
+++ src/components/Atoms/Buttons/Button-Footer.tsx
@@ -1,28 +1,21 @@
+import Link from "next/link";
 import * as React from "react";
 
 interface ButtonProps {
-  className: string;
+  icon: string;
   href: string;
-  target: string;
-  rel: string;
-  alt: string;
   title: string;
 }
 
-export const ButtonIconFooter = ({
-  className,
-  href,
-  alt,
-  title,
-}: ButtonProps) => {
+export const ButtonIconFooter = ({ icon, href, title }: ButtonProps) => {
   return (
     <button
       className="hover:bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)] text-slate-200 hover:text-transparent hover:bg-clip-text shadow-lg text-2xl lg:text-3xl h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none lg:mr-2"
       type="button"
     >
-      <a href={href} target="_blank" rel="noreferrer" alt={alt} title={title}>
-        <i className={className} aria-hidden="true"></i>
-      </a>
+      <Link href={href} target="_blank" rel="noreferrer" title={title}>
+        <i className={icon} aria-hidden="true"></i>
+      </Link>
     </button>
   );
 };
