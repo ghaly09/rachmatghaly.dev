e821eb0f598e92300cb4c4879303fce7568d8219
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-27 01:33:51 +0700

    Modified: Mapping button to footer

--- src/components/Molecules/Footer/index.tsx
+++ src/components/Molecules/Footer/index.tsx
@@ -1,5 +1,36 @@
+import { footerConfig } from "@/src/config/footer-config";
 import React from "react";
+import { ButtonIconFooter } from "../../Atoms/Buttons/Button-Footer";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  //   console.log(footerConfig.buttonAttributes);
+
+  return (
+    <footer className="pt-8 pb-2 px-5 lg:px-0">
+      <div className="container mx-auto">
+        <hr className="my-6 border-slate-400" />
+        <div className="flex flex-wrap justify-center text-slate-200 font-ghaly text-center lg:text-left">
+          Let&apos;s keep in touch!
+        </div>
+        <div className="text-center mt-2 mb-10">
+          {footerConfig.buttonAttributes.map((attribute, index) => (
+            <ButtonIconFooter
+              key={index}
+              icon={attribute.icon}
+              href={attribute.url}
+              title={attribute.title}
+            />
+          ))}
+        </div>
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full md:w-4/12 px-4 mx-auto text-center">
+            <p className="font-ghaly text-sm text-slate-200 font-semibold">
+              Copyright &copy; 2022 by{" "}
+              <span className="hover:text-[#00CFC9]">Rachmat Ghaly</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
