5f1fe9b88e46610c320d82da7e650e9be2dd77fc
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-06 19:51:18 +0700

    Modified: update navbar link from navLink and navConfig

--- src/components/Molecules/Navbar/index.tsx
+++ src/components/Molecules/Navbar/index.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React from "react";
+import NavLink from "./NavLink";
+import { navConfig } from "@/src/config/navbar-config";
 
 export default function Navbar() {
   return (
-    <div>
-      <p className="text-white">Navbar</p>
-    </div>
+    <nav>
+      <div className="flex flex-row gap-4 items-center bg-[#0e1111] text-white font-semibold text-lg h-12 py-4 w-auto lg:max-w-5xl px-5 lg:px-0 lg:mx-auto">
+        {navConfig.map((link, index) => (
+          <NavLink href={link.href} page={link.page} key={index} />
+        ))}
+      </div>
+    </nav>
   );
 }
