4b6d4075215f3eead21e2bc7a1404b06962c56c3
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-07 23:51:04 +0700

    Modified: Update index.tsx for using Dark Mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 import Layout from "../components/Templates/Layout";
+import Script from "next/script";
 // import LandingPageTemplate from "../../templates/LandingPage/LandingPageTemplate";
 
 const Index: NextPage = () => {
@@ -13,11 +14,11 @@ const Index: NextPage = () => {
           href="https://user-images.githubusercontent.com/60167960/229319877-77600973-dc1f-4b2a-b0da-ed38a67eec0e.png"
         />
         <meta name="description" content="Created by Rachmat Ghaly." />
-        <script
-          src="https://kit.fontawesome.com/0ae515efac.js"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
+      <Script
+        src="https://kit.fontawesome.com/0ae515efac.js"
+        crossOrigin="anonymous"
+      />
       <Layout>
         <div>{/* <p className="text-white">Page home ghaly</p> */}</div>
       </Layout>
