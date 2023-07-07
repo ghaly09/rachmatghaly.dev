1dcfb158ffa3c17431c276ba7535f1b8670a3c1d
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-07 23:52:28 +0700

    Modified: Update Layout for using Dark Mode

--- src/components/Templates/Layout/index.tsx
+++ src/components/Templates/Layout/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import Footer from "../../Molecules/Footer";
 import Header from "../../Molecules/Header";
+import { ThemeProvider } from "../Theme/theme-provider";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,13 +11,15 @@ export default function Layout(props: LayoutProps) {
   const { children } = props;
 
   return (
-    <main>
-      <div className="h-2 bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]"></div>
-      <div className="max-w-[68rem] mx-auto">
-        <Header />
-        <div>{children}</div>
-        <Footer />
-      </div>
-    </main>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <main>
+        <div className="h-2 bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]"></div>
+        <div className="max-w-[68rem] mx-auto">
+          <Header />
+          <div>{children}</div>
+          <Footer />
+        </div>
+      </main>
+    </ThemeProvider>
   );
 }
