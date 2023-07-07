b2ec4f20e1f8d97bace06b0e6dce6ebbb2ee442a
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-08 00:34:07 +0700

    Modified: Change Button bg color & Border-color

--- src/components/Templates/Theme/ModeToggle.tsx
+++ src/components/Templates/Theme/ModeToggle.tsx
@@ -18,13 +18,16 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className="dark:bg-[#0e1111]">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent
+        align="end"
+        className="dark:bg-[#0e1111] border-input"
+      >
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
