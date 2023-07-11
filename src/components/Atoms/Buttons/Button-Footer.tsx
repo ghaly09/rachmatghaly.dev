36ce36495dcd4f43e1c6404f3946875ed21dbe32
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 21:08:04 +0700

    Modified: update contact icon color for ThemeMode

--- src/components/Atoms/Buttons/Button-Footer.tsx
+++ src/components/Atoms/Buttons/Button-Footer.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 export const ButtonIconFooter = ({ icon, href, title }: ButtonProps) => {
   return (
     <button
-      className="hover:bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)] text-slate-200 hover:text-transparent hover:bg-clip-text shadow-lg text-2xl lg:text-3xl h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none lg:mr-2"
+      className="hover:bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)] text-gray-600 dark:text-slate-200 hover:text-transparent hover:bg-clip-text shadow-lg text-2xl lg:text-3xl h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none lg:mr-2"
       type="button"
     >
       <Link href={href} target="_blank" rel="noreferrer" title={title}>
