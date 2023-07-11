ca11cfaa6febd9a8fb1ab8e4e70dd1eb5ee4c5ff
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 19:58:34 +0700

    Modified: change Header max-width, margin, padding and make it adaptive to all devices

--- src/components/Molecules/Navbar/index.tsx
+++ src/components/Molecules/Navbar/index.tsx
@@ -7,7 +7,7 @@ import { ModeToggle } from "../../Templates/Theme/ModeToggle";
 export default function Navbar() {
   return (
     <nav>
-      <div className="flex flex-row gap-4 items-center dark:text-white font-semibold text-lg h-12 py-4 w-auto lg:max-w-5xl px-5 lg:px-0 lg:mx-auto">
+      <div className="flex flex-row gap-4 items-center dark:text-white font-semibold text-lg h-12 py-4 w-auto px-5 lg:px-0 lg:mx-auto">
         {navConfig.map((link, index) => (
           <NavLink href={link.href} page={link.page} key={index} />
         ))}
