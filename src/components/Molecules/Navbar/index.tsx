c7a3f0057692d8db1151cb2f16128e1e4940bf9d
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 20:39:28 +0700

    Modified: change justifiy-between for navLink and ThemeMode

--- src/components/Molecules/Navbar/index.tsx
+++ src/components/Molecules/Navbar/index.tsx
@@ -6,13 +6,13 @@ import { ModeToggle } from "../../Templates/Theme/ModeToggle";
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className="flex flex-row gap-4 items-center dark:text-white font-semibold text-lg h-12 py-4 w-auto px-5 lg:px-0 lg:mx-auto">
+    <nav className="flex flex-row items-center justify-between dark:text-white font-semibold text-lg h-15 py-4 w-auto px-5 lg:px-0 lg:mx-auto">
+      <div className="flex flex-row gap-4">
         {navConfig.map((link, index) => (
           <NavLink href={link.href} page={link.page} key={index} />
         ))}
-        <ModeToggle />
       </div>
+      <ModeToggle />
     </nav>
   );
 }
