4de344fb49231b2d13ed7d05b551e4ff07eaccd0
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 21:07:06 +0700

    Modified: update Header layout for responsive design

--- src/components/Molecules/Header/index.tsx
+++ src/components/Molecules/Header/index.tsx
@@ -3,7 +3,8 @@ import Navbar from "../Navbar";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full">
+    <header className="sticky top-0 z-50 w-full h-15 dark:bg-[#0e1111]">
+      <div className="sticky top-0 z-50 w-full h-2 bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]"></div>
       <Navbar />
     </header>
   );
