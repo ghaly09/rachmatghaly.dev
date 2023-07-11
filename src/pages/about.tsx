83a52ef6c042f7a5ab63481ee3290fd145a6825d
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 06:54:24 +0700

    Modified: Update About Motions, Color Theme, add skills & Contact Me

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
-import AboutContainer from "../components/Molecules/Containers/AboutContainer";
+import AboutContainer from "../components/Templates/Containers/AboutContainer";
 import AboutDescMotion from "../components/ui/FramerMotion/aboutDesc-motion";
 import AboutDesc from "../components/Atoms/Descriptions/AboutDesc";
 import GhalyPhoto from "../components/Atoms/Photos/Ghaly";
+import AboutTitle from "../components/Atoms/Descriptions/AboutTitle";
+import AboutTitleMotion from "../components/ui/FramerMotion/aboutTitle-motion";
+import SkillsContainer from "../components/Templates/Containers/SkillsContainer";
+import SkillsIcon from "../components/Molecules/ChildContainers/SkillsIcon-container";
+import ContactContainer from "../components/Templates/Containers/ContactContainer";
+import ContactDesc from "../components/Atoms/Descriptions/ContactDesc";
 
 export default function About() {
   return (
-    <section>
+    <section id="about">
+      {/* About Me */}
+      <AboutTitleMotion>
+        <AboutTitle />
+      </AboutTitleMotion>
       <AboutContainer>
         <AboutDescMotion>
           <AboutDesc />
@@ -15,6 +25,16 @@ export default function About() {
           <GhalyPhoto />
         </AboutDescMotion>
       </AboutContainer>
+
+      {/* Current favorite tech stack/tools */}
+      <SkillsContainer>
+        <SkillsIcon />
+      </SkillsContainer>
+
+      {/* Contact Me */}
+      <ContactContainer>
+        <ContactDesc />
+      </ContactContainer>
     </section>
   );
 }
