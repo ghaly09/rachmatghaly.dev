60dd2176060cd6061755a544e1bea197ad2216ba
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 06:53:00 +0700

    Modified: Update AboutTitle Motions

--- src/components/Atoms/Descriptions/AboutTitle.tsx
+++ src/components/Atoms/Descriptions/AboutTitle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AboutTitle() {
   return (
     <div className="text-center sm:text-start px-5 lg:px-0">
-      <h2 className="text-xl lg:text-3xl text-gray-600 dark:text-white font-bold pt-20 md:pt-20">
+      <h2 className="text-xl lg:text-3xl text-black dark:text-white font-bold pt-20 md:pt-20">
         About
       </h2>
       <h2 className="text-2xl lg:text-4xl font-bold pb-3 text-transparent bg-clip-text bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]">
