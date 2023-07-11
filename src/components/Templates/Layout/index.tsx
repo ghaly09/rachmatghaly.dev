680fccad5562e031a5ec0ac1f8e5b2e8b9b932f8
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 20:38:33 +0700

    Modified: change Padding and gap

--- src/components/Templates/Layout/index.tsx
+++ src/components/Templates/Layout/index.tsx
@@ -29,7 +29,7 @@ const Layout = (props: LayoutProps) => {
 
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <main>
-          <div className="h-2 bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]"></div>
+          <div className="sticky top-0 z-50 w-full h-2 bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]"></div>
           <div className="max-w-[76rem] mx-auto">
             <Header />
             <div>{children}</div>
