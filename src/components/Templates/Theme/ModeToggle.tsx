0ef62b896d6df1711b89e2e73b5969bbad9b7f01
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 20:40:18 +0700

    Modified: change Hover and border outline of ThemeMode

--- src/components/Templates/Theme/ModeToggle.tsx
+++ src/components/Templates/Theme/ModeToggle.tsx
@@ -18,9 +18,13 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="dark:bg-[#0e1111]">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          variant="outline"
+          size="themeModeIcon"
+          className="dark:bg-[#0e1111] dark:border-slate-700 hover:dark:border-[rgb(0,218,210)] hover:text-[rgb(0,218,210)]"
+        >
+          <Sun className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
