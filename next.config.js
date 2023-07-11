7c2858d79eb45f033f694298cef01191a0fac0a6
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 05:10:59 +0700

    Config: Add hostnames config for image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "user-images.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
