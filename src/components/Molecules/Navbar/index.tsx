e01a092d4a8e631e0a82fb215b2341c5d429870b
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 20:44:37 +0700

    Modified: change NavLink gap for responsive design

--- src/components/Molecules/Navbar/index.tsx
+++ src/components/Molecules/Navbar/index.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
-import React from "react";
-import NavLink from "./NavLink";
 import { navConfig } from "@/src/config/navbar-config";
 import { ModeToggle } from "../../Templates/Theme/ModeToggle";
+import NavLink from "./NavLink";
 
 export default function Navbar() {
   return (
     <nav className="flex flex-row items-center justify-between dark:text-white font-semibold text-lg h-15 py-4 w-auto px-5 lg:px-0 lg:mx-auto">
-      <div className="flex flex-row gap-4">
+      <div className="flex flex-row gap-4 lg:gap-6">
         {navConfig.map((link, index) => (
           <NavLink href={link.href} page={link.page} key={index} />
         ))}
