3f536370ebd919d234ff3473067046bf1ebd36a0
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 05:17:36 +0700

    Atoms: make description for about page

---
+++ src/components/Atoms/Descriptions/AboutDesc.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function AboutDesc() {
+  return (
+    <>
+      <p
+        id="text"
+        className="font-ghaly text-base pb-3 text-gray-600 dark:text-[#d3d4d6] "
+      >
+        Hello, I'm Ghaly. I am a physics student enthusiastic about research,
+        teaching, programming, and design activities to build visually
+        attractive applications with user-friendly interactions.{" "}
+      </p>
+      <p className="font-ghaly text-base pb-4 text-gray-600 dark:text-[#d3d4d6] ">
+        I started learning web development in January 2022, I've been fascinated
+        by how software works and how it can be used to solve complex problems
+        and make a positive impact on people's lives. So, I decided to make
+        software engineering to be my professional field.
+        <br />
+        Initially, I had no prior experience in web development, but I was
+        determined to learn and hone my skills. To achieve this, I turned to
+        various learning resources such as YouTube, Coursera, and the Ruangguru
+        Bootcamp especially{" "}
+        <span className="border-dotted border-b-2 border-[rgb(61,224,102)] hover:text-red-500 dark:hover:text-[rgb(61,224,102)]">
+          frontend development.
+        </span>
+      </p>
+
+      <p className="font-ghaly text-base pb-4 text-gray-600 dark:text-[#d3d4d6] ">
+        {" "}
+        There are a lot of things and technologies to learn in frontend
+        development and that motivated me to explore more challenges. I enjoy
+        learning something new and doing research as well as collaborating with
+        small teams and big teams by getting feedback to improve myself better.
+      </p>
+      <p className="font-ghaly text-base pb-4 text-gray-600 dark:text-[#d3d4d6] ">
+        In this website I will be writing some blogs and showcase my projects. I
+        believe that writing what I have learned is the best way to remember
+        things, and I can share my knowledge along the way. So do contact me and
+        I will be very happy to help!
+      </p>
+    </>
+  );
+}
