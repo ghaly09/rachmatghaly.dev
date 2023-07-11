09fd84c5bcb31d62b3c72997cfe4077f81d9b338
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-11 19:57:52 +0700

    Modified: change Header max-width, margin, sticky and make it adaptive to all devices

--- src/components/Molecules/Header/index.tsx
+++ src/components/Molecules/Header/index.tsx
@@ -3,7 +3,7 @@ import Navbar from "../Navbar";
 
 export default function Header() {
   return (
-    <header className="fixed w-full">
+    <header className="sticky top-0 z-50 w-full">
       <Navbar />
     </header>
   );
