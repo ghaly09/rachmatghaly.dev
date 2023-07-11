043268a71e36d5272356bf45e0d5c05fc1d7e534
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 05:12:29 +0700

    Modified: Update text-color for light mode

--- src/components/Molecules/Navbar/index.tsx
+++ src/components/Molecules/Navbar/index.tsx
@@ -4,7 +4,7 @@ import NavLink from "./NavLink";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row items-center justify-between dark:text-white font-semibold text-lg h-15 py-4 w-auto px-5 lg:px-0 max-w-[76rem] mx-auto">
+    <nav className="flex flex-row items-center justify-between dark:text-white font-semibold text-lg h-15 py-4 w-auto px-5 lg:px-0 max-w-[68rem] mx-auto">
       <div className="flex flex-row gap-4 lg:gap-6">
         {navConfig.map((link, index) => (
           <NavLink href={link.href} page={link.page} key={index} />
