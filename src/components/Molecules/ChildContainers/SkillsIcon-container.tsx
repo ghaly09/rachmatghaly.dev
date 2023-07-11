02779809a84994bad72ad0a42db5b6e54e2fa5e6
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 06:51:17 +0700

    Component: Make Contact component

---
+++ src/components/Molecules/ChildContainers/SkillsIcon-container.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import IconSkill from "../../Atoms/Icons/IconSkills";
+import { aboutConfig } from "@/src/config/about-config";
+import Image from "next/image";
+
+export default function SkillsIcon() {
+  return (
+    <div className="flex flex-wrap items-center gap-2 mt-1">
+      <span className="flex">
+        {aboutConfig.iconSkills?.slice(0, 4).map((icon, index) => (
+          <IconSkill
+            key={index}
+            style={icon?.style}
+            src={icon?.src}
+            width={icon?.width}
+            height={icon?.height}
+            alt={icon?.alt}
+            title={icon?.title}
+          />
+        ))}
+      </span>
+
+      {aboutConfig.iconSkills?.slice(4, 8).map((icon, index) => (
+        <IconSkill
+          key={index}
+          style={icon?.style}
+          src={icon?.src}
+          width={icon?.width}
+          height={icon?.height}
+          alt={icon?.alt}
+          title={icon?.title}
+        />
+      ))}
+
+      <div>
+        {" "}
+        <Image
+          src="https://user-images.githubusercontent.com/60167960/226206827-2b005282-0b89-4bf0-9e68-f48ad671db96.svg"
+          width="20"
+          height="45"
+          alt="verticalLine"
+        />
+      </div>
+      {aboutConfig.iconSkills?.slice(8, 10).map((icon, index) => (
+        <IconSkill
+          key={index}
+          style={icon?.style}
+          src={icon?.src}
+          width={icon?.width}
+          height={icon?.height}
+          alt={icon?.alt}
+          title={icon?.title}
+        />
+      ))}
+    </div>
+  );
+}
