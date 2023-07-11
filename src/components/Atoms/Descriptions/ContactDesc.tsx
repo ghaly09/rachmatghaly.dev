73f57c4cbdde55f16fe8b4e45a1e091318d1c7d6
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-12 06:50:26 +0700

    Component: Make ChildContainers

---
+++ src/components/Atoms/Descriptions/ContactDesc.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ContactDesc() {
+  return (
+    <div>
+      <h2 className="text-xl lg:text-3xl text-black dark:text-white font-bold ">
+        Contact
+      </h2>
+      <p className="font-ghaly text-gray-600 dark:text-[#d3d4d6] pt-3">
+        Do contact me for collaboration, especially frontend works. I’ll be
+        happy to help! (find my email in the footer)
+      </p>
+    </div>
+  );
+}
