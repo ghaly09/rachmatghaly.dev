992f49dfe6cc56e06da6b5a0446d38787b40930f
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-13 23:43:11 +0700

    Modified: Change container import file laocations

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import AboutContainer from "../components/Templates/Containers/AboutContainer";
+import AboutContainer from "../components/Templates/Containers/About-Page/AboutContainer";
 import AboutDescMotion from "../components/ui/FramerMotion/aboutDesc-motion";
 import AboutDesc from "../components/Atoms/Descriptions/AboutDesc";
 import GhalyPhoto from "../components/Atoms/Photos/Ghaly";
 import AboutTitle from "../components/Atoms/Descriptions/AboutTitle";
 import AboutTitleMotion from "../components/ui/FramerMotion/aboutTitle-motion";
-import SkillsContainer from "../components/Templates/Containers/SkillsContainer";
+import SkillsContainer from "../components/Templates/Containers/About-Page/SkillsContainer";
 import SkillsIcon from "../components/Molecules/ChildContainers/SkillsIcon-container";
-import ContactContainer from "../components/Templates/Containers/ContactContainer";
+import ContactContainer from "../components/Templates/Containers/About-Page/ContactContainer";
 import ContactDesc from "../components/Atoms/Descriptions/ContactDesc";
 
 export default function About() {
