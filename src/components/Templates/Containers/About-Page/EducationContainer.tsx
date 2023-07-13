528f52f1c91b8c2ebd73822448369edd16e08e27
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-13 23:44:45 +0700

    Modified: Make EducationConatainer and move other container to new folder about container

---
+++ src/components/Templates/Containers/About-Page/EducationContainer.tsx
@@ -0,0 +1,18 @@
+import { container } from "@/src/lib/utils/Types";
+import { motion } from "framer-motion";
+
+export default function EducationContainer(props: container) {
+  const { children } = props;
+
+  return (
+    <motion.div
+      className="px-5 lg:px-0 pt-20"
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -10, opacity: 0 }}
+      transition={{ duration: 0.8, delay: 0.6 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
