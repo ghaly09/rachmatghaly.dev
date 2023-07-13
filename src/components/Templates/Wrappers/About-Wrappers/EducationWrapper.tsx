e0d5c4ec363a52d8b9e4bdc253764425b5aacb1f
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-14 05:06:59 +0700

    Modified: Move Motion Education Wrapper to TabsEduContainer and delete Education COntainer

--- src/components/Templates/Wrappers/About-Wrappers/EducationWrapper.tsx
+++ src/components/Templates/Wrappers/About-Wrappers/EducationWrapper.tsx
@@ -1,17 +1,7 @@
 import { wrapper } from "@/src/lib/utils/Types";
-import { motion } from "framer-motion";
 
 export default function EducationWrapper(props: wrapper) {
-  const { delay = 0.6, duration = 0.8, children } = props;
+  const { children } = props;
 
-  return (
-    <motion.div
-      initial={{ y: 10, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      exit={{ y: -10, opacity: 0 }}
-      transition={{ duration: duration, delay: delay }}
-    >
-      {children}
-    </motion.div>
-  );
+  return <div className="px-5 lg:px-0 pt-20">{children}</div>;
 }
