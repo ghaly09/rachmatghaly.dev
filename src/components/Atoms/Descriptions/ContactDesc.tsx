e4b0860abecb078495d098efbdc71603629cfb4c
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-14 02:28:57 +0700

    Desc: Update Desc for using Reveal Motion

--- src/components/Atoms/Descriptions/ContactDesc.tsx
+++ src/components/Atoms/Descriptions/ContactDesc.tsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { Reveal } from "../../ui/FramerMotion/onReveal";
 
 export default function ContactDesc() {
   return (
     <div>
-      <h2 className="text-xl lg:text-3xl text-black dark:text-white font-bold ">
-        Contact
-      </h2>
-      <p className="font-ghaly text-gray-600 dark:text-[#d3d4d6] pt-3">
-        Do contact me for collaboration, especially frontend works. I’ll be
-        happy to help! (find my email in the footer)
-      </p>
+      <Reveal delay={0.2} duration={0.5}>
+        <h2 className="text-xl lg:text-3xl text-black dark:text-white font-bold ">
+          Contact
+        </h2>
+      </Reveal>
+      <Reveal delay={0.3} duration={0.5}>
+        <p className="font-ghaly text-gray-600 dark:text-[#d3d4d6] pt-3">
+          Do contact me for collaboration, especially frontend works. I’ll be
+          happy to help! (find my email in the footer)
+        </p>
+      </Reveal>
     </div>
   );
 }
