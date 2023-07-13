b61dbd51eb95a838c36d6280ded8d5819b5cb750
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-14 01:54:05 +0700

    Desc: Update About Desc

--- src/components/Templates/Wrappers/About-Wrappers/EducationWrapper.tsx
+++ src/components/Templates/Wrappers/About-Wrappers/EducationWrapper.tsx
@@ -1,7 +1,11 @@
 import { wrapper } from "@/src/lib/utils/Types";
 import { motion } from "framer-motion";
 
-export default function EducationWrapper(props: wrapper) {
+export default function EducationWrapper(
+  props: wrapper,
+  delay: number = 0.6,
+  duration: number = 0.8
+) {
   const { children } = props;
 
   return (
@@ -9,7 +13,7 @@ export default function EducationWrapper(props: wrapper) {
       initial={{ y: 10, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       exit={{ y: -10, opacity: 0 }}
-      transition={{ duration: 0.8, delay: 0.6 }}
+      transition={{ duration: duration, delay: delay }}
     >
       {children}
     </motion.div>
