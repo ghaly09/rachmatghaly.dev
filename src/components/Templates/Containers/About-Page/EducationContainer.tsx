73f119fdcbbc9e42a59ac9e4ab0d21fabf903931
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-14 05:06:15 +0700

    Modified: Move Motion Education container to TabsEduContainer

--- src/components/Templates/Containers/About-Page/EducationContainer.tsx
+++
@@ -1,7 +0,0 @@
-import { container } from "@/src/lib/utils/Types";
-
-export default function EducationContainer(props: container) {
-  const { children } = props;
-
-  return <div className="px-5 lg:px-0 pt-20">{children}</div>;
-}
