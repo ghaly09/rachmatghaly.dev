da95cf3fc6105f4fa80f10037e8e7fed3d1b6665
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-14 02:25:08 +0700

    AboutPage: Move Motion to container compoents using REVEAL in view from framer motions

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,13 @@
-import * as React from "react";
-import AboutContainer from "../components/Templates/Containers/About-Page/AboutContainer";
-import AboutDescMotion from "../components/ui/FramerMotion/aboutDesc-motion";
 import AboutDesc from "../components/Atoms/Descriptions/AboutDesc";
-import GhalyPhoto from "../components/Atoms/Photos/Ghaly";
 import AboutTitle from "../components/Atoms/Descriptions/AboutTitle";
-import AboutTitleMotion from "../components/ui/FramerMotion/aboutTitle-motion";
-import SkillsContainer from "../components/Templates/Containers/About-Page/SkillsContainer";
-import SkillsIcon from "../components/Molecules/ChildContainers/SkillsIcon-container";
-import ContactContainer from "../components/Templates/Containers/About-Page/ContactContainer";
 import ContactDesc from "../components/Atoms/Descriptions/ContactDesc";
-import { Reveal } from "../components/ui/FramerMotion/onReveal";
+import GhalyPhoto from "../components/Atoms/Photos/Ghaly";
+import SkillsIcon from "../components/Molecules/ChildContainers/SkillsIcon-container";
+import AboutContainer from "../components/Templates/Containers/About-Page/AboutContainer";
+import ContactContainer from "../components/Templates/Containers/About-Page/ContactContainer";
+import SkillsContainer from "../components/Templates/Containers/About-Page/SkillsContainer";
+import AboutDescMotion from "../components/ui/FramerMotion/aboutDesc-motion";
+import AboutTitleMotion from "../components/ui/FramerMotion/aboutTitle-motion";
 
 export default function About() {
   return (
@@ -35,11 +33,10 @@ export default function About() {
       {/* Educations */}
 
       {/* Contact Me */}
-      <Reveal>
-        <ContactContainer>
-          <ContactDesc />
-        </ContactContainer>
-      </Reveal>
+
+      <ContactContainer>
+        <ContactDesc />
+      </ContactContainer>
     </section>
   );
 }
