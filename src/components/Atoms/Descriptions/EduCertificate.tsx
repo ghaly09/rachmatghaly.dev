b273cc725f1674bf62b276560fdcfe0419aef19f
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-15 20:53:37 +0700

    Decs: Add Educations & Certificates Descriptions

---
+++ src/components/Atoms/Descriptions/EduCertificate.tsx
@@ -0,0 +1,13 @@
+interface PropTypes {
+  desc?: string;
+}
+
+export default function EduCertificateDesc({ desc }: PropTypes) {
+  return (
+    <div>
+      <p className="font-ghaly text-gray-600 dark:text-[#d3d4d6] mt-3 mb-4">
+        {desc}
+      </p>
+    </div>
+  );
+}
