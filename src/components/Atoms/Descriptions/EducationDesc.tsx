c4294999cc303a33ba1cea96afdca2abaa71e55c
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-15 20:38:35 +0700

    Desc: Update Desc

--- src/components/Atoms/Descriptions/EducationDesc.tsx
+++ src/components/Atoms/Descriptions/EducationDesc.tsx
@@ -5,7 +5,7 @@ interface PropTypes {
 export default function EducationDesc({ desc }: PropTypes) {
   return (
     <div>
-      <p className="font-ghaly text-gray-600 dark:text-[#d3d4d6] pt-3">
+      <p className="font-ghaly text-gray-600 dark:text-[#d3d4d6] mt-3 mb-4">
         {desc}
       </p>
     </div>
