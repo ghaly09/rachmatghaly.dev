4bf28f5d4d22003e2d9340d8c405d3bd93bae979
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-17 06:44:03 +0700

    Config: change description projects

--- src/config/projects-config.ts
+++ src/config/projects-config.ts
@@ -16,7 +16,7 @@ export const projectsConfig: projectTypes = {
       imgSrc:
         "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
       description:
-        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
       technologies: [],
       date: "July 13, 2021",
       directLink: "",
@@ -26,7 +26,7 @@ export const projectsConfig: projectTypes = {
       imgSrc:
         "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
       description:
-        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
       technologies: [],
       date: "July 13, 2021",
       directLink: "",
@@ -36,7 +36,7 @@ export const projectsConfig: projectTypes = {
       imgSrc:
         "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
       description:
-        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
       technologies: [],
       date: "July 13, 2021",
       directLink: "",
@@ -46,7 +46,37 @@ export const projectsConfig: projectTypes = {
       imgSrc:
         "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
       description:
-        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "A design of application and device for helping someone to get or looking for his/her lost stuff efficiently.",
       technologies: [],
       date: "July 13, 2021",
       directLink: "",
