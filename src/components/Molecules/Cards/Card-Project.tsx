eec705ce1bd5b296a76a581b5fa05f2973a861d7
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-17 06:45:36 +0700

    Cards: Update Line Effect and Width of Cards projects to be Responsive

--- src/components/Molecules/Cards/Card-Project.tsx
+++ src/components/Molecules/Cards/Card-Project.tsx
@@ -2,6 +2,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/src/components/ui/card";
@@ -10,6 +11,7 @@ import * as React from "react";
 import { Reveal } from "../../ui/FramerMotion/onReveal";
 import Link from "next/link";
 import { PropProjectTypes } from "@/src/lib/Types";
+import SeeMore from "../../Atoms/Buttons/Button-SeeMore";
 
 export default function CardProject({
   title,
@@ -19,13 +21,20 @@ export default function CardProject({
   date,
   directLink,
 }: PropProjectTypes): React.JSX.Element {
+  const [lineHover, setLineHover] = React.useState("");
+
   return (
-    <Card className="flex flex-col-reverse w-fit sm:max-w-[250px] lg:max-w-[350px] justify-between font-ghaly rounded-[6px] active:scale-[0.90] border-gray-300 dark:border-gray-500 dark:bg-[#0e1111] hover:scale-[1.02] hover:duration-300 animate-shadow hover:shadow-lg">
+    <Card
+      className="flex flex-col-reverse w-fit sm:max-w-[250px] lg:max-w-[350px] justify-between font-ghaly rounded-[6px] active:scale-[0.90] border-gray-300 dark:border-gray-500 dark:bg-[#0e1111] hover:scale-[1.02] hover:duration-300 animate-shadow hover:shadow-lg"
+      onMouseOver={() => setLineHover("line-effect")}
+      onMouseLeave={() => setLineHover("")}
+    >
       <CardHeader className="p-4">
         <CardTitle>{title}</CardTitle>
         <CardDescription className="w-auto dark:text-[#d3d4d6]">
           {description}
         </CardDescription>
+        <SeeMore directLink={directLink} lineEffect={lineHover} />
       </CardHeader>
 
       <CardContent className="p-0">
@@ -36,8 +45,6 @@ export default function CardProject({
           height={300}
           alt={title}
         />
-
-        <Link href={directLink} target="_blank" rel="noreferrer"></Link>
       </CardContent>
     </Card>
   );
