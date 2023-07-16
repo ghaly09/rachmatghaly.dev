be833b0d26ef644079eb9258c3b9cb2fc9674c49
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-17 05:19:21 +0700

    Config: Add some data of projects

---
+++ src/config/projects-config.ts
@@ -0,0 +1,55 @@
+export interface projectTypes {
+  dataProjects: Array<{
+    title: string;
+    description: string;
+    technologies: string[];
+    date: string;
+    imgSrc: string;
+    directLink: string;
+  }>;
+}
+
+export const projectsConfig: projectTypes = {
+  dataProjects: [
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+    {
+      title: "UI/UX FindThing Project",
+      imgSrc:
+        "https://user-images.githubusercontent.com/60167960/230802874-23718e05-6d5b-42ce-bc12-e0171027498a.png",
+      description:
+        "An design of application and device for helping someone to get or looking for his/her lost stuff efficiently by detecting its location or when someone finds your stuff.",
+      technologies: [],
+      date: "July 13, 2021",
+      directLink: "",
+    },
+  ],
+};
