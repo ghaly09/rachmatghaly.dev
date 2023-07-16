25306216bd3db50294152e6bf03ca86504e248fb
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-17 05:17:28 +0700

    Comp: Make Project Descriptions component

---
+++ src/components/Atoms/Descriptions/ProjectsDesc.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Reveal } from "../../ui/FramerMotion/onReveal";
+
+export default function ProjectsDesc() {
+  return (
+    <div className="font-ghaly px-5 lg:px-0 mt-6 md:mt-12">
+      <Reveal delay={0.2} duration={0.5}>
+        <h2 className="text-3xl lg:text-5xl font-bold pb-3 text-transparent bg-clip-text bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]">
+          Projects
+        </h2>
+      </Reveal>
+      <Reveal delay={0.3} duration={0.5}>
+        <p className="text-base lg:text-md pb-3 text-gray-600 dark:text-[#d3d4d6]">
+          Showcase of my works on Frontend Development and Design.
+        </p>
+      </Reveal>
+    </div>
+  );
+}
