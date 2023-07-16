7fa2293e187a5dcbadf1e834b692ab7e63b55661
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-16 20:40:13 +0700

    Nav: Apply Active Link Style in NavLink

--- src/components/Molecules/Navbar/NavLink.tsx
+++ src/components/Molecules/Navbar/NavLink.tsx
@@ -1,11 +1,24 @@
 import { navProps } from "@/src/config/navbar-config";
+import { cn } from "@/src/lib/utils";
 import Link from "next/link";
-import React from "react";
+import * as React from "react";
 
-export default function NavLink({ href, page }: navProps) {
+export default function NavLink({ href, page, style, onclick }: navProps) {
+  // const [active, setActive] = React.useState({ style: "", page });
+
+  // React.useEffect(() => {
+  //   style = "";
+  // }, []);
+
+  // console.log(active.page);
+
   return (
     <Link
-      className="hover:text-transparent focus:text-transparent focus:bg-clip-text hover:bg-clip-text hover:bg-[rgb(61,224,102)] focus:bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]"
+      className={cn(
+        "hover:text-transparent hover:bg-clip-text hover:bg-[rgb(61,224,102)]",
+        style
+      )}
+      onClick={onclick}
       href={href}
     >
       {page}
