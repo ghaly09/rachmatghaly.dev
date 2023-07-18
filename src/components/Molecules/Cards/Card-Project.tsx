659f188db97aaf0ff4a8320fcc49fa3089e4e962
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-18 19:49:11 +0700

    Atoms: Add icons tech/stack

--- src/components/Molecules/Cards/Card-Project.tsx
+++ src/components/Molecules/Cards/Card-Project.tsx
@@ -2,16 +2,15 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/src/components/ui/card";
-import Image from "next/image";
-import * as React from "react";
-import { Reveal } from "../../ui/FramerMotion/onReveal";
-import Link from "next/link";
 import { PropProjectTypes } from "@/src/lib/Types";
+import Image from "next/image";
+import Link from "next/link";
+import * as React from "react";
 import SeeMore from "../../Atoms/Buttons/Button-SeeMore";
+import { IconTech } from "../../Atoms/Icons/IconTech";
 
 export default function CardProject({
   title,
@@ -23,28 +22,51 @@ export default function CardProject({
 }: PropProjectTypes): React.JSX.Element {
   const [lineHover, setLineHover] = React.useState("");
 
+  const handleLinkClick = (url: string) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+    link.click();
+  };
+
+  const handleDirectLink = () => {
+    handleLinkClick(directLink);
+  };
+
   return (
     <Card
-      className="flex flex-col-reverse w-fit sm:max-w-[250px] lg:max-w-[350px] justify-between font-ghaly rounded-[6px] active:scale-[0.90] border-gray-300 dark:border-gray-500 dark:bg-[#0e1111] hover:scale-[1.02] hover:duration-300 animate-shadow hover:shadow-lg"
+      className="flex flex-col-reverse w-fit sm:max-w-[250px] lg:max-w-[350px] justify-between font-ghaly rounded-[6px] cursor-pointer active:scale-[0.90] border-gray-300 dark:border-gray-500 dark:bg-[#0e1111] hover:scale-[1.02] hover:duration-300 animate-shadow hover:shadow-lg"
       onMouseOver={() => setLineHover("line-effect")}
       onMouseLeave={() => setLineHover("")}
+      onClick={handleDirectLink}
     >
+      {" "}
       <CardHeader className="p-4">
+        {/* Icons Tech */}
+        <div className="flex flex-row">
+          {technologies.map((tech: any, index: number) => (
+            <IconTech key={index} icon={tech.icon} iconName={tech.iconName} />
+          ))}
+        </div>
+
         <CardTitle>{title}</CardTitle>
-        <CardDescription className="w-auto dark:text-[#d3d4d6]">
+
+        <CardDescription className="w-auto dark:text-[#d3d4d6] line-clamp-3">
           {description}
         </CardDescription>
         <SeeMore directLink={directLink} lineEffect={lineHover} />
       </CardHeader>
-
       <CardContent className="p-0">
-        <Image
-          className={`object-cover w-screen max-h-[400px] sm:w-[350px] sm:max-h-[200px] rounded-t-sm cursor-pointer`}
-          src={imgSrc}
-          width={350}
-          height={300}
-          alt={title}
-        />
+        <div className="w-fit">
+          <Image
+            className={`object-cover w-screen max-h-[400px] sm:w-[350px] sm:max-h-[180px] rounded-t-sm cursor-pointer`}
+            src={imgSrc}
+            width={350}
+            height={300}
+            alt={title}
+          />
+        </div>
       </CardContent>
     </Card>
   );
