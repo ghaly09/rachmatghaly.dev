ab6a99e2ad6b703348d03df9a9bc2b7126f47f4d
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-18 19:53:57 +0700

    Nav: Update NavLink for Active page

--- src/components/Molecules/Navbar/index.tsx
+++ src/components/Molecules/Navbar/index.tsx
@@ -2,15 +2,19 @@ import { navConfig } from "@/src/config/navbar-config";
 import { ModeToggle } from "../../Templates/Theme/ModeToggle";
 import NavLink from "./NavLink";
 import * as React from "react";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
   const styleActive =
     "text-transparent bg-clip-text bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]";
 
   const [active, setActive] = React.useState({
     style: styleActive,
-    pageName: "Home",
+    pageName: router.pathname,
   });
+
+  // console.log(router.pathname);
 
   return (
     <nav className="flex flex-row flex-wrap items-center justify-between dark:text-white font-semibold text-lg h-15 py-4 w-auto px-5 lg:px-0 max-w-[68rem] mx-auto">
@@ -20,11 +24,11 @@ export default function Navbar() {
             key={index}
             href={link.href}
             page={link.page}
-            style={link.page != active.pageName ? "" : active.style}
+            style={link.href != active.pageName ? "" : active.style}
             onclick={() => {
               setActive({
                 ...active,
-                pageName: link.page,
+                pageName: link.href,
               });
             }}
           />
