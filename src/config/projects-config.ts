d587e5c6771e9dd5e4f8b6e0fcf4ba5d862c4f54
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-23 22:26:49 +0700

    config: Update StayDoors banner Project

--- src/config/projects-config.ts
+++ src/config/projects-config.ts
@@ -34,7 +34,7 @@ export const projectsConfig: projectTypes = {
     {
       title: "StayDoors Mobile",
       imgSrc:
-        "https://res.cloudinary.com/druckyjuu/image/upload/v1690088046/Projects/StayDoors_svutpf.svg",
+        "https://res.cloudinary.com/druckyjuu/image/upload/v1690123952/Projects/StayDoors_lemzur.svg",
       description:
         "A Hotels Reservation App using React Native for finding and booking many Hotels and Destinations.",
       technologies: [
