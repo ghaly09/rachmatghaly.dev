40d2ed9cffddb41d35525d7af93a2ac534d95592
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-24 02:16:53 +0700

    feat(NavLink): Update Navlink Active status

--- src/components/Molecules/Navbar/NavLink.tsx
+++ src/components/Molecules/Navbar/NavLink.tsx
@@ -3,14 +3,13 @@ import { cn } from "@/src/lib/utils";
 import Link from "next/link";
 import * as React from "react";
 
-export default function NavLink({ href, page, style, onclick }: navProps) {
+export default function NavLink({ href, page, style }: navProps) {
   return (
     <Link
       className={cn(
         "hover:text-transparent hover:bg-clip-text hover:bg-[rgb(61,224,102)]",
         style
       )}
-      onClick={onclick}
       href={href}
     >
       {page}
