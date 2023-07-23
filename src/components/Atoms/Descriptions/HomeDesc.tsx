cf00a18bd1232f09cdf403b3e0fae62f2dc32a5d
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-24 01:24:28 +0700

    Atoms(HomeDesc): Add Motions

--- src/components/Atoms/Descriptions/HomeDesc.tsx
+++ src/components/Atoms/Descriptions/HomeDesc.tsx
@@ -1,5 +1,21 @@
 import * as React from "react";
 import { SlideReveal } from "../../ui/FramerMotion/slideReveal";
+import { Button } from "../../ui/button";
+import { BsFillFileEarmarkTextFill } from "react-icons/Bs";
+import Link from "next/link";
+import { motion, useReducedMotion } from "framer-motion";
+import clsx from "clsx";
+
+const animation = {
+  hide: {
+    x: -16,
+    opacity: 0,
+  },
+  show: {
+    x: 0,
+    opacity: 1,
+  },
+};
 
 export default function HomeDesc() {
   const [ghalyEffect, setGhalyEffect] = React.useState("");
@@ -15,6 +31,32 @@ export default function HomeDesc() {
       }, 10000);
     }
   }, [ghalyEffect]);
+
+  const shouldReduceMotion = useReducedMotion();
+
+  let isFreeVariants = {
+    hide: {
+      x: 0,
+      opacity: 1,
+    },
+    show: {
+      x: -48,
+      opacity: 0,
+    },
+  };
+
+  if (shouldReduceMotion) {
+    isFreeVariants = {
+      hide: {
+        x: 0,
+        opacity: 1,
+      },
+      show: {
+        x: 0,
+        opacity: 0,
+      },
+    };
+  }
 
   return (
     <div className="mt-32">
@@ -38,6 +80,52 @@ export default function HomeDesc() {
           attractive applications with user-friendly interactions.
         </p>
       </SlideReveal>
+
+      <div className="flex flex-row gap-5 mt-5">
+        <Button
+          className="bg-[#0e1111] border-[1px] border-slate-600 font-ghaly rounded-sm"
+          variant={"outline"}
+        >
+          More about me
+        </Button>
+
+        <Link
+          href={"https://www.linkedin.com/in/rachmat-ghaly/"}
+          target="_blank"
+          className="flex flex-row items-center font-ghaly font-semibold cursor-pointer text-slate-300 hover:text-white"
+        >
+          <motion.div
+            className={clsx("flex gap-2")}
+            initial="hide"
+            animate="show"
+          >
+            <motion.div
+              variants={animation}
+              transition={{ delay: 2.8 }}
+              className={clsx("relative z-10")}
+            >
+              <motion.div
+                variants={isFreeVariants}
+                transition={{ delay: 4 + 1.5, duration: 0.4 }}
+              >
+                AVAILABLE FOR HIRE
+              </motion.div>
+
+              <motion.div
+                initial={{ x: -48, opacity: 0, pointerEvents: "none" }}
+                animate={{ x: 0, opacity: 1, pointerEvents: "auto" }}
+                transition={{ delay: 4 + 1.6, duration: 0.4 }}
+                className={clsx(
+                  "absolute top-0 left-0 flex flex-row items-center"
+                )}
+              >
+                <BsFillFileEarmarkTextFill className="mr-2 h-[18px] w-[18px]" />{" "}
+                RESUME
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </Link>
+      </div>
     </div>
   );
 }
