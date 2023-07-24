39a859a255e1f41248d320dde1b3a61f1694533e
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-24 14:33:13 +0700

    fix(Bugs): Button-Footer.tsx

--- src/components/Atoms/Buttons/Button-Footer.tsx
+++ src/components/Atoms/Buttons/Button-Footer.tsx
@@ -13,7 +13,7 @@ export const ButtonIconFooter = ({ icon, href, title }: ButtonProps) => {
       className="hover:bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)] text-current hover:text-transparent hover:bg-clip-text shadow-lg text-2xl lg:text-3xl h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none lg:mr-2"
       type="button"
     >
-      <Link href={href} target="_blank" rel="noreferrer" title={title}>
+      <Link href={href} target="_blank" rel="noreferrer" title={title} passHref>
         <i className={icon} aria-hidden="true"></i>
       </Link>
     </button>
