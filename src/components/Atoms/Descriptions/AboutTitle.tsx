eaeeeeed4d59af13b941cfe5ca6555838d2d40a5
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-25 04:24:17 +0700

    feat(color): update text color base Title About

--- src/components/Atoms/Descriptions/AboutTitle.tsx
+++ src/components/Atoms/Descriptions/AboutTitle.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function AboutTitle() {
   return (
-    <div className="text-center sm:text-start px-5 lg:px-0">
-      <h2 className="text-xl lg:text-3xl text-black dark:text-white font-bold pt-20 md:pt-20">
+    <div className="flex flex-col items-center sm:items-start px-5 lg:px-0 mt-20 md:mt-20">
+      <h2 className="text-xl lg:text-3xl text-black dark:text-white font-bold">
         About
       </h2>
-      <h2 className="text-2xl lg:text-4xl font-bold pb-3 text-transparent bg-clip-text bg-gradient-to-r from-[rgb(0,218,210)] to-[rgb(61,224,102)]">
+      <h2 className="text-2xl lg:text-4xl font-bold pb-3 max-w-[280px] text-transparent bg-clip-text color-ghaly">
         Rachmat Ghaly
       </h2>
     </div>
