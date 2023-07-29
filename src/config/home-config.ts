aa218670084433d38f48280f88b844e627274e8e
ghaly09 <rachmatghaly0997@gmail.com>
2023-07-29 08:11:18 +0700

    config(home): Update Resume Direct Link

--- src/config/home-config.ts
+++ src/config/home-config.ts
@@ -15,7 +15,7 @@ export interface homeTypes {
 
 export const homeConfig: homeTypes = {
   resume:
-    "https://drive.google.com/file/d/1zvRZvlQYO11jRdWIMu1xdn2C1vm2SDHY/view?usp=sharing",
+    "https://drive.google.com/file/d/1ztPdj9MBwLDdZzYWgsYdJstZr7teRGOw/view?usp=sharing",
   iconsLink: [
     {
       icon: FiFigma,
