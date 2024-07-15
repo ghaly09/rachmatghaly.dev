5354bab9305b940b361457dfaae405e85cbc0a71
ghaly09 <rachmatghaly0997@gmail.com>
2024-07-15 22:42:01 +0700

    feat(comp): add Skeleton to certificate cards for loading

--- src/components/Molecules/Cards/Card-Certificate.tsx
+++ src/components/Molecules/Cards/Card-Certificate.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import * as React from "react";
 import { Reveal } from "../../ui/FramerMotion/onReveal";
 import Link from "next/link";
+import { Skeleton } from "../../ui/skeleton";
 
 interface propType {
   title: string;
@@ -27,6 +28,7 @@ export default function CardCertificate({
     hover: "opacity-0",
     offset: "opacity-100",
   });
+  const [imageLoaded, setImageLoaded] = React.useState(false);
 
   return (
     <Card className="flex flex-col w-[260px] justify-between font-ghaly rounded-[6px] border-gray-300 dark:border-gray-500 dark:bg-[#0e1111] hover:scale-[1.02] hover:duration-300 animate-shadow hover:shadow-lg">
@@ -50,12 +52,16 @@ export default function CardCertificate({
                 setEffect({ hover: "opacity-0", offset: "opacity-100" });
               }}
             >
+              {!imageLoaded && (
+                <Skeleton className="absolute top-0 z-30 w-full h-[120px] rounded-sm cursor-pointer" />
+              )}
               <Image
                 className={`object-fill h-[120px] rounded-sm cursor-pointer ${effect.offset}`}
                 src={imgLink}
                 width={220}
                 height={200}
                 alt={title}
+                onLoad={() => setImageLoaded(true)}
               />{" "}
               <h5
                 className={`absolute top-12 left-[80px] cursor-pointer font-bold text-white ${effect.hover} opacity-0 duration-300`}
