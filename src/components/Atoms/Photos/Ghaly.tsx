a2c23bda913de1459363d26569152a032ddc9420
ghaly09 <rachmatghaly0997@gmail.com>
2024-07-15 22:41:41 +0700

    feat(comp): add Skeleton to photo for loading

--- src/components/Atoms/Photos/Ghaly.tsx
+++ src/components/Atoms/Photos/Ghaly.tsx
@@ -1,15 +1,24 @@
 import { aboutConfig } from "@/src/config/about-config";
 import Image from "next/image";
 import React from "react";
+import { Skeleton } from "../../ui/skeleton";
 
 export default function GhalyPhoto() {
+  const [imageLoaded, setImageLoaded] = React.useState(false);
   return (
-    <Image
-      className={aboutConfig.photoGhaly.style}
-      src={aboutConfig.photoGhaly.src}
-      alt={aboutConfig.photoGhaly.alt}
-      width={1000}
-      height={1000}
-    />
+    <div>
+      {!imageLoaded && (
+        <Skeleton className="rounded-md w-60 h-[370px] md:w-[315px]" />
+      )}
+
+      <Image
+        className={aboutConfig.photoGhaly.style}
+        src={aboutConfig.photoGhaly.src}
+        alt={aboutConfig.photoGhaly.alt}
+        width={1000}
+        height={1000}
+        onLoad={() => setImageLoaded(true)}
+      />
+    </div>
   );
 }
