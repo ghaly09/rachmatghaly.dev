db876a2b9957cb67a6277bcb211049cd3fc0e20d
ghaly09 <rachmatghaly09@gmail.com>
2025-06-02 00:08:12 +0700

    modified: change Copyright to 2025

--- src/components/Molecules/Footer/index.tsx
+++ src/components/Molecules/Footer/index.tsx
@@ -1,12 +1,13 @@
 import { footerConfig } from "@/src/config/footer-config";
 import React from "react";
 import { ButtonIconFooter } from "../../Atoms/Buttons/Button-Footer";
+import { texts } from "@/src/constants/texts.constant";
 
 export default function Footer() {
   return (
     <footer className="pt-8 pb-2 px-5 lg:px-0">
       <div className="mx-auto">
-        <hr className="my-6 border-slate-400" />
+        <hr className="my-6 border-[0.1px] border-gray-500" />
         <div className="flex flex-wrap justify-center text-gray-600 dark:text-slate-200 font-ghaly text-center lg:text-left">
           Let&apos;s keep in touch!
         </div>
@@ -23,8 +24,8 @@ export default function Footer() {
         <div className="flex flex-wrap items-center md:justify-between justify-center">
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <p className="font-ghaly text-sm text-gray-600 dark:text-slate-200 font-semibold">
-              Copyright &copy; 2024 by{" "}
-              <span className="hover:text-[#00CFC9]">Rachmat Ghaly</span>
+              Copyright &copy; {texts.copyrightDate} by{" "}
+              <span className="hover:text-[#00CFC9]">{texts.name}</span>
             </p>
           </div>
         </div>
